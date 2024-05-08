0ffa60f7ee526e8829f89e6e55f6f295ee881c3d
Matii <166016459+Matiitii@users.noreply.github.com>
2024-05-08 20:49:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,193 @@
+/* 1 Ejercicio 
+
+
+
+let numeroUsuario = Number (prompt( '¡Ingrese un número!'))
+let numeroUsuario1 = Number (prompt( '¡Ingrese otro número!'))
+
+
+if (numeroUsuario > numeroUsuario1){
+    alert (numeroUsuario + ' es mayor a ' + numeroUsuario1)
+} else {
+    alert (numeroUsuario1 + ' es mayor a ' + numeroUsuario)
+}
+
+
+*/
+
+/*2 Ejercicio 
+
+
+
+let numeroUsuario2 = Number(prompt ('¡Ingrese un número!'))
+
+
+if (numeroUsuario2 > 0){
+    alert ('¡Tu número es positivo!')
+} else if ( numeroUsuario2 < 0) {
+ alert ('¡Tu número es negativo!') 
+} else {
+    alert ('¡Tu número es 0!')
+}
+
+
+*/
+
+
+/*3 Ejercicio 
+
+
+let numeroUsuario3 = Number(prompt('¡Ingese un número!'))
+
+if (numeroUsuario3 % 2 == 0) {
+    alert ('¡Tu número es par!')
+} else {
+    alert ('¡Tu número es impar!')
+} 
+
+
+*/
+
+
+/*Ejercicio 4
+
+
+let edadUsuario4 = Number (prompt ('¡Ingrese su edad!'))
+
+
+if ( edadUsuario4 >= 18){
+    alert ('¡Sos mayor de edad!' )
+} else {
+    alert ('¡No sos mayor de edad!')
+}
+
+*/
+
+/*Ejercicio 5
+
+
+let lunes = 1
+let martes = 2
+let miercoles = 3
+let jueves = 4
+let viernes = 5
+let sabado = 6
+let domingo = 7
+
+let numeroUsuario5 = Number (prompt ('¡Ingrese un número!'))
+
+if (numeroUsuario5 == lunes){
+    alert ('¡Es lunes!')
+} else
+    if (numeroUsuario5 == martes){
+        alert ('¡Es martes!')
+    } else if (numeroUsuario5 == miercoles){
+        alert ('¡Es miercoles!')
+    } else if (numeroUsuario5 == jueves){
+        alert ('¡Es jueves!')
+    } else if (numeroUsuario5 == viernes){
+        alert ('¡Es viernes!')
+    } else if (numeroUsuario5 == sabado){
+        alert ('¡Es sabado!')
+    } else if (numeroUsuario5 == domingo){
+        alert ('¡Es domingo!')
+    }
+
+    */
+
+
+    /*Ejercicio 6
+
+
+
+    let numeroUsuario6 = Number(prompt('¡Ingese un número!'))
+
+if ((numeroUsuario6 % 3 == 0) && (numeroUsuario6 % 5== 0)){
+    alert (numeroUsuario6 + ' si es divisible por 3 y 5')
+} else {
+    alert (numeroUsuario6 + ' no es divisible por 3 y 5')
+}
+
+
+*/
+
+
+
+/*Ejercicio 7
+
+let numeroUsuario7 = Number(prompt('¡ingrese un número!'))
+
+if ((numeroUsuario7 < 100 == true) && (numeroUsuario7 >= 0)) {
+alert ('Tu numero es menor a 100 y es positivo!')} else {
+    alert ('Tu numero no es menor a 100 y/o no es positivo')
+}
+
+
+*/
+
+
+/*Ejercicio 8
+
+let genero = prompt('¡Ingrese su genero!')
+
+let edad = Number (prompt ('Ingrese su edad!'))
+
+if ((genero == 'masculino') && (edad >= 65)) {
+    alert ('¡Puede jubilarse!') 
+} else if ((genero == 'femenino') && (edad >= 60)) {
+    alert ('¡Puede jubilarse!')
+} else {
+    alert ('¡No puede jubilarse!')
+}
+
+*/
+
+
+/*Ejercicio 9
+
+
+let numero1 = Number (prompt( '¡Ingrese un número!'))
+let numero2 = Number (prompt( '¡Ingrese otro número!'))
+let numero3 = Number (prompt( '¡Ingrese un último número!'))
+
+if (((numero1 == numero2) || (numero1 == numero3)) ||(numero2 == numero3)) {
+    alert ('Dos o más números son iguales!')
+} else {
+    alert ('No hay números iguales')
+}
+
+*/
+
+/*Ejercicio 10
+
+
+let numeroUsuario8 = Number(prompt('Ingese un numero'))
+
+if ((numeroUsuario8 % 4 == 0) && (numeroUsuario8 % 6== 0)){
+    alert (numeroUsuario8 + ' si es divisible por 4 y 6')
+} else {
+    alert (numeroUsuario8 + ' no es divisible por 4 y 6')
+}
+
+if ((numeroUsuario8 % 9 == 0) && (numeroUsuario8 % 12== 0)){
+    alert (numeroUsuario8 + ' si es divisible por 9 y 12')
+} else {
+    alert (numeroUsuario8 + ' no es divisible por 9 y 12')
+}
+
+*/
+
+/*Ejercicio 11
+
+
+let edad1 = Number (prompt( '¡Ingrese una edad!'))
+let edad2 = Number (prompt( '¡Ingrese otra edad!'))
+let edad3 = Number (prompt( '¡Ingrese una última edad!'))
+
+if ((edad1 >= 18) || (edad2 >= 18) || (edad3 >= 18)) {
+    alert ('¡Una o más edades son mayores a 18!')
+} else {
+    alert ('¡No hay edades mayores a 18!')
+}
+
+*/
